dbff22b1b049a29759a5b4a76f68ee1074bad10e
Sonuil <atharvap506@gmail.com>
2026-08-10 15:12:49 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api/tickets/";
-
+const API_URL = "https://supportflow-9hor.onrender.com/api/tickets/";
 /* =========================
    HELPERS
 ========================= */
